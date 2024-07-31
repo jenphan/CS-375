9c337549cbccb18a2472fbd3cc591a8d82e3ae38
Jen <jdp386@drexel.edu>
2024-07-31 13:31:11 -0400

    Create quizcreator.html

--- app/server.js
+++ app/server.js
@@ -6,6 +6,18 @@ let port = 3000;
 let hostname = "localhost";
 app.use(express.static("../public"));
 
+let quizzes = [];
+
+app.post('/createquiz', (req, res) => {
+    const quiz = req.body
+    quizzes.push(quiz)
+    res.status(200).json(quiz)
+})
+
+app.get('/createquiz', (req, res) => {
+    res.json(quiz)
+})
+
 app.listen(port, hostname, () => {
     console.log(`http://${hostname}:${port}`);
 });
