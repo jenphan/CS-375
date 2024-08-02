f5b1c17b4b990c38d1531d62191919737385913c
slb522 <slb522@tux.cs.drexel.edu>
2024-08-02 18:51:37 -0400

    add input validation checks

--- public/script/register-course.js
+++ public/script/register-course.js
@@ -9,9 +9,38 @@ document.getElementById('create-course-form').addEventListener('submit', functio
     if (!courseName || !subjectCode || !courseNumber || !crn) {
       alert('Please fill in all fields');
       return false;
-  }
+    }
+    if (isNaN(subjectCode)) {
+        alert('Subject code must be a number');
+        return false;
+    }
+    if (isNaN(courseNumber)) {
+        alert('Course number must be a number');
+        return false;
+    }
+    if (isNaN(crn)) {
+        alert('CRN must be a number');
+        return;
+    }
+
+    if (courseName.length > 50) {
+      alert('Course name is too long');
+      return false;
+    }
+    if (subjectCode.length > 5) {
+        alert('Subject code is too long');
+        return false;
+    }
+    if (courseNumber.length > 3) {
+        alert('Course number is too long');
+        return false;
+    }
+    if (crn.length != 5) {
+        alert('CRN must be 5 digits');
+        return false;
+    }
   
-    alert(`${courseName} is created!`);
+    alert(`${courseName} has been created!`);
     console.log("ok\n");
     window.location.href = './prof.html';
   });
