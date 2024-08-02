3e89cf0156fe463eb7251085e6529d074756b700
slb522 <slb522@tux.cs.drexel.edu>
2024-08-02 18:44:40 -0400

    add courses table

--- setup.sql
+++ setup.sql
@@ -8,7 +8,7 @@ CREATE TABLE users (
     email VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(10) NOT NULL
-);
+); 
 
 -- Insert initial data
 INSERT INTO users (username, email, password, role) VALUES
@@ -16,6 +16,17 @@ INSERT INTO users (username, email, password, role) VALUES
 ('jane_doe', 'jd123@drexel.edu','wordpass', 'professor');
 
 
+CREATE TABLE courses (
+    id SERIAL PRIMARY KEY,
+    course_name VARCHAR(50) NOT NULL,
+    subject_code VARCHAR(5) NOT NULL,
+    course_number INT(3) NOT NULL,
+    crn INT(5) NOT NULL
+);
+
+INSERT INTO courses (course_name, subject_code, course_number, crn) VALUES
+('Web Development', 'CS', 375, 12345),
+('Data Structures', 'CS', 260, 54321);
 
 -- Open psql cmd: psql -U your_username
 -- Create the database: CREATE DATABASE CS375;
