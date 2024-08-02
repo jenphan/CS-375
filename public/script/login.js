e0a5008be048e3b538d89f58063d40a191e737d4
Lam Nguyen <lambhg3456@gmail.com>
2024-08-02 15:01:11 -0400

    Integrad Linh auth to new login and register interface

--- public/script/login.js
+++ public/script/login.js
@@ -9,12 +9,27 @@ document.getElementById('login-form').addEventListener('submit', function(event)
       alert('Please enter both username and password.');
       return;
     }
+    let data = {
+      username: username,
+      password: password
+    };
+
+    fetch('auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
   
-    // Simulate login process
-    if (username === 'admin' && password === 'password') {
-      alert('Login successful!');
-      // Redirect or perform other actions here
-    } else {
-      alert('Invalid username or password.');
-    }
+    }).then(data =>{
+      console.log('Success', data);
+  
+    }).catch(error => {
+      console.log('Error', error);
+    });
   });
--- public/script/register.js
+++ public/script/register.js
@@ -1,13 +1,14 @@
+const { response } = require("express");
+
 document.getElementById('create-account-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting
   
     const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirm-password').value;
     const role = document.getElementById('register-role').value;
 
-    if (!username || !email || !password || !confirmPassword || !role) {
+    if (!username || !password || !confirmPassword || !role) {
       alert('Please fill in all fields');
       return false;
   }
@@ -19,9 +20,31 @@ document.getElementById('create-account-form').addEventListener('submit', functi
   
     // Simulate account creation process
     alert(`Account created for ${username}!
-    Email: ${email}
     Role: ${role}`);
-    console.log("ok\n");
     // Redirect to login or home page
+    let data = {
+      username: username,
+      password: password,
+      role: role
+    };
+
+    fetch('auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+  
+    }).then(data =>{
+      console.log('Success', data);
+  
+    }).catch(error => {
+      console.log('Error', error);
+    });
     window.location.href = '../index.html'; // Change this to your desired page
   });
