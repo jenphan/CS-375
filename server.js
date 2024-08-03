8d1288f6853ddb2e32773d5c5c1b403286aa219a
mal483 <mal483@drexel.edu>
2024-08-03 18:34:36 -0400

    basic database functions
    created query.js which contains functions to exclusively send query requests to the database. currently supports creating and updating accounts, creating and updating courses, and course enrollment. lists of accounts and lists of courses can be retrieved.

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ let port = 3000;
 let hostname = "localhost";
 app.use(express.static("public"));
 
+
+/* database stuff */
+const pg = require("pg");
+const env = require("./env.json");
+const Pool = pg.Pool;
+const pool = new Pool(env);
+pool.connect().then(function () {
+  console.log(`Connected to database ${env.database}`);
+});
+
+
+
 app.listen(port, hostname, () => {
     console.log(`http://${hostname}:${port}`);
 });
