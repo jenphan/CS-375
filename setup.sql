b66b281006b898e9b6e469cf3ab8e3d270512354
slb522 <slb522@tux.cs.drexel.edu>
2024-08-03 12:17:58 -0400

    remove email column

--- setup.sql
+++ setup.sql
@@ -16,7 +16,6 @@ DROP TABLE IF EXISTS courses;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    email VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(10) NOT NULL
 ); 
