11c934ef267d0daea71c04be79dea0ae237de3b3
Linh Nguyen <npl02042000@gmail.com>
2024-08-05 09:33:18 -0400

    Update server.js for logout function

--- app/server.js
+++ app/server.js
@@ -1,8 +1,10 @@
 let axios = require("axios");
 const { response } = require("express");
 let express = require("express");
+let session = require("express-session");
 
 const bodyParser = require('body-parser');
+const crypto = require('crypto'); // Import crypto for generating a random string
 const authRoutes = require('../routes/auth');
 const quizRoutes = require('../routes/quiz');
 const createCourse = require('../routes/course');
@@ -11,13 +13,24 @@ let app = express();
 let port = 3000;
 let hostname = "localhost";
 
+// Generate a random secret key
+const secretKey = crypto.randomBytes(64).toString('hex');
+
+// Session setup
+app.use(session({
+    secret: secretKey,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Set secure: true if using HTTPS
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("../public"));
 
 // Routes
 app.use('/auth', authRoutes);
-app.use('/quiz', quizRoutes)
+app.use('/quiz', quizRoutes);
 app.use('/course', createCourse);
 
 app.use((err, req, res, next) => {
