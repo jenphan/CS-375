25fbcc01c080604b963c73c082b2965c965b54ed
slb522 <slb522@tux.cs.drexel.edu>
2024-08-10 00:39:02 -0400

    remove comments

---
+++ public/script/addCourse.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const addCourseButton = document.getElementById('add-course');
+    const inputContainer = document.getElementById('inputContainer');
+
+    addCourseButton.addEventListener('click', function() {
+        if (inputContainer.classList.contains('hidden')) {
+            inputContainer.classList.remove('hidden');
+        } else {
+            inputContainer.classList.add('hidden');
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const courseCodeInput = document.getElementById('course-code');
+    const username = data.user.username;
+
+
+    inputField.addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+            const courseCode = courseCodeInput.value.trim();
+            if (courseCode) {
+                enroll(username, courseCode);
+            }
+        }
+    });
+});
