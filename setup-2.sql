48f1c797b676ce77941934fac3393fb6604b7ba7
Linh Nguyen <npl02042000@gmail.com>
2024-08-12 14:31:43 -0400

    fix syntax and add initial database for checking calendar page

--- setup-2.sql
+++ setup-2.sql
@@ -1,14 +1,15 @@
 DROP DATABASE IF EXISTS cs375;
 DROP DATABASE IF EXISTS blueboard;
-DROP DATABASE IF EXISTS cs375blueboard
+DROP DATABASE IF EXISTS cs375blueboard;
 CREATE DATABASE cs375blueboard;
-\c blueboard;
+\c cs375blueboard;
 
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS courses CASCADE;
 DROP TABLE IF EXISTS enrollment CASCADE;
 DROP TABLE IF EXISTS quizzes CASCADE;
-DROP TABLE IF EXISTS submissions CASCADE;
+DROP TABLE IF EXISTS quiz_questions CASCADE;
+DROP TABLE IF EXISTS quiz_responses CASCADE;
 DROP TABLE IF EXISTS appointments CASCADE;
 
 CREATE TABLE users (
@@ -53,3 +54,15 @@ CREATE TABLE appointments (
     title VARCHAR(255) NOT NULL,
     date TIMESTAMPTZ NOT NULL
 );
+
+-- Insert initial data
+INSERT INTO users (username, password, role) VALUES
+('abc', '$argon2id$v=19$m=65536,t=3,p=4$T/3Mid5O4H2tdaTdIVV35w$4n6nPs2ANYiPz77w64biA1jN7uZMpxee2EqPwQbBAVE', 'student'); -- password: password
+
+INSERT INTO quizzes (title, creator, deadline, timer) VALUES
+('Web Development Quiz 1', 1, '2024-08-07 23:59:00', 60),
+('Data Structures Quiz 1', 1, '2024-08-10 23:59:00', 60);
+
+INSERT INTO appointments (title, date) VALUES
+('Meeting with John', '2024-08-05 10:00:00'),
+('Project Discussion', '2024-08-06 15:00:00');
